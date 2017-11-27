6b01e6447716ad7efd15da0ff643c9e78cee0c66
Christian Zenaty <cazenaty@yahoo.com>
2017-11-27 14:36:52 -0800

    updated header

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const express = require("express"),
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/poll", (req, res) => {
+  res.render("poll");
+});
+
+app.get("/login", (req, res) => {
+  res.send("Login page will be here");
+});
+
+app.get("/register", (req, res) => {
+  res.send("Registration");
 });
 
 const PORT = process.env.PORT || 3000;
