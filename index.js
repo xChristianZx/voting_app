17dbda76e9cb0995259844ecb9123499d611b662
Christian Zenaty <cazenaty@yahoo.com>
2017-11-28 10:40:07 -0800

    user registration and sync with mongodb

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
-  app = express();
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  User = require("./models/user");
 
+const app = express();
+mongoose.connect("mongodb://localhost/voting_app", { useMongoClient: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
+//HOME
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+//POLL
 app.get("/poll", (req, res) => {
   res.render("poll");
 });
 
+//LOGIN
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
+//REGISTER
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+//Create New User
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const newUser = {
+    username: username,
+    password: password
+  };
+
+  User.create(newUser, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(` You have been registered ${newlyCreated.username}`);
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/secret", (req, res) => {
